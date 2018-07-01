304281b0dcf06fd77e9c792625d32206188f7acd
Luisa Bolado <mboladop@hotmail.com>
2018-07-01 17:59:45 +0000

    walking skeleton, disaDisallowedHost error when deployed fixed

--- pardobymireia/settings/base.py
+++ pardobymireia/settings/base.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ['mireia-ecommerce-mboladop.c9users.io', ]
+ALLOWED_HOSTS = ['mireia-ecommerce-mboladop.c9users.io', 'mboladop-mireia-ecommerce.herokuapp.com' ]
 
 
 # Application definition
