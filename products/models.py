25e9ca69f5737937ece27cb791d69b17580bf164
mboladop <mboladop@gmail.com>
2018-08-20 23:35:53 +0100

    adding category to product models.py

--- products/models.py
+++ products/models.py
@@ -3,11 +3,19 @@ from django.db.models import Avg
 
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Title")
+    def get_products(self):
+        return Product.objects.filter(category=self)
+    
+    def __str__(self):
+        return self.title
+
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
-   
+    category = models.ForeignKey(Category, verbose_name="Category", on_delete= models.PROTECT)
     def __str__(self):
         return self.name
