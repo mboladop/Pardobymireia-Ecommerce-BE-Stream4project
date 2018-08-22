c04503c589b39b718246433ff3dcb99410a5165d
mboladop <mboladop@gmail.com>
2018-08-22 22:18:03 +0100

    product profile

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.conf.urls import include, url
-from .views import get_products, search, category_detail
+from .views import get_products, search, category_detail, product_profile
 
 urlpatterns = [
     path('<pk>/', category_detail, name='category_detail'),
     path('', get_products, name='get_products'),
+    path('profile/<pk>/', product_profile, name='product_profile'),
     # path('', search , name='search'),
 ]
--- products/views.py
+++ products/views.py
@@ -5,14 +5,29 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+
 def get_products(request):
     products = Product.objects.all()
     return render(request, "products/products.html", {'products': products})
-    
-def search(request):
-    products = Product.objects.filter(name__icontains=request.GET['query'])
-    return render(request, "products/products.html", {"products": products})
 
 def category_detail(request, pk):
     products = get_list_or_404(Product, category=pk)
     return render(request, 'products/products.html', {"products": products, "current": int(pk)})
+
+
+def product_profile(request, pk):
+    """
+    Create a view that returns a single
+    product object based on the item ID (pk) and
+    render it to the 'product_profile.html' template.
+    Or return a 404 error if the product is
+    not found
+    """
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "products/product_profile.html", {'product': product})
+
+
+def search(request):
+    products = Product.objects.filter(name__icontains=request.GET['query'])
+    return render(request, "products/products.html", {"products": products})
+
