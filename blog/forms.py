17598f0db64fc367c917e7ac5cd209f67e9a0e43
mboladop <mboladop@gmail.com>
2018-08-23 20:41:14 +0100

    blog styled

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from .models import Blog
 
 class BlogPostForm(forms.ModelForm):
+    
+    title = forms.CharField(label='Título')
+    content = forms.CharField(label='Texto')
+    image = forms.ImageField(label='imagen')
+    tag = forms.CharField(label='Etiquetas(#)')
+    
     class Meta:
         model = Blog
         fields = ('title', 'content', 'image', 'tag')
