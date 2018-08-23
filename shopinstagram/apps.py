bccbca8e903c28fa5c1b2be8541d059c7c6dbdae
mboladop <mboladop@gmail.com>
2018-08-23 16:34:17 +0100

    shopinstagram app created

---
+++ shopinstagram/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ShopinstagramConfig(AppConfig):
+    name = 'shopinstagram'
---
+++ shopinstagram/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def shopinstagram(request):
+    return render(request, "shopinstagram/shopinstagram.html")
+
